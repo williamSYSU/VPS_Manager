29a225b514e02f292434c3962881179651a1baca
William <278976237@qq.com>
2018-08-07 11:03:02 +0800

    update code

--- bash/setip.sh
+++ bash/setip.sh
@@ -2,19 +2,13 @@
 
 opt=$1
 ip=$2
-target=$3
 
 if [ $opt != "I" -a $opt != "D" ]; then
     echo "非法操作，输入：I或D"
     exit 1
 fi
 
-if [ $target != "REJECT" -a $target != "ACCEPT" ]; then
-    echo "非法目标，输入：REJECT或ACCEPT"
-    exit 1
-fi
-
-iptables -$opt INPUT -s $ip -j $target
+iptables -$opt INPUT -s $ip -j REJECT 
 
 if [ ! -d "/etc/iptables-rules" ]; then
     mkdir /etc/iptables-rules
@@ -22,5 +16,5 @@ fi
 iptables-save > /etc/iptables-rules/rules.ban
 cp -f /etc/iptables-rules/rules.ban /root/vps-server/rules.ban
 
-echo "execute: iptables -$opt INPUT -s $ip -j $target"
+echo "execute: iptables -$opt INPUT -s $ip -j REJECT"
 
